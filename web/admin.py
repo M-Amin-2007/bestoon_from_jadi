2753de026d87660afe94f130f372290c34318987
M-Amin-2007 <mamp2007.mohammadi@gmail.com>
2023-06-19 11:08:10 +0430

    create expense model

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,7 @@
+"""manage admin panel"""
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+reg = admin.site.register
+reg(Expense)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,14 @@
+"""database models"""
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Expense(models.Model):
+    """model expense"""
+    date = models.DateTimeField()
+    text = models.CharField(max_length=200)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.amount} in {self.date}"
